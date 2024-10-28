cd1f5f575a2295feaf32d6c23c9cecc61f337cb6
farnoosh <daarcparsa@yahoo.com>
2024-10-28 11:04:24 +0330

    complete app - by generate and show new question

--- src/components/ParentApp/ParentApp.jsx
+++ src/components/ParentApp/ParentApp.jsx
@@ -4,12 +4,16 @@ import { useUserInfoContext } from "../Contexts/UserInfoContext";
 import Question from "../Question/Question";
 import './style.scss';
 import { useAnswerContext } from "../Contexts/AnswerContext";
+import { Button } from "../Button/Button";
+
+const question=[{question:"food"},{question:"old"},{question:"color"}]
 
 const ParentApp = () => {
 
 
     const [answers, setAnswers] = useState([]);
     const {test, setTest} = useUserInfoContext();
+    const[count,setCount]=useState(0);
 
     const {userA, setUserA} = useAnswerContext();
     
@@ -36,19 +40,34 @@ const addAnswer = (answerItem) => {
     ]
     setAnswers(newAnswer);
     setUserA(newUserA);
+    setCount(prev=>prev+1)
     console.log(userA);
     
 }
 
-useEffect(() => {
-    console.log('userAnswer:', userA)
-}, [userA])
+// useEffect(() => {
+//     if(count>=3){
+//         alert('ok')
+//     }
+// }, [count])
+
+const handleAnswers = () => {
+
+    console.log("user name:", test[0].name)
+    console.log("answers:")
+
+    userA.map((item) => {
+        console.log(item.userAnswer)
+    })
+    
+}
 
     return(
         <div className="ParentApp">
-      {test[0].name !== "" ? <Question addAnswer={addAnswer} question={"How old are you?"}/> : ""}
-      {userA[0].userAnswer &&<Question addAnswer={addAnswer} question={"What is your favorite food?"}/>}
-    
+        {test[0] && <Question addAnswer={addAnswer} question={"How old are you?"}/>}
+        {userA[1] && <Question addAnswer={addAnswer} question={"What is your favorite food?"}/>}
+        {userA[2] && <Question addAnswer={addAnswer} question={"What is your favorite color?"}/>}
+        {userA[3] && <Button handleClick={handleAnswers} title={"Send Answers"}/>}
         </div>
     )
 }
